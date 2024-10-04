ca47c0ffbb5eeff532a5f4700f1c3c5d809e6cc7
ella-an2 <ella.an@mths.ca>
2024-10-04 15:03:43 +0000

    did some coding

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,23 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ella
+Created on: Oct 2024
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from time import*
 
+display.clear
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+let cookie_click_number: number
+cookie_click_number = 0
+
+while True:
+    if button_a. is_pressed
+        cookie_click_number += 1
+        time(1)
+        display.scroll(str(cookie_click_number))
+
+while True:
+    if button_b. is_pressed
